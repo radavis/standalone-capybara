43e978ab6677e7a372a7128c5e52065ad77002be
Richard Davis <rdavis.bacs@gmail.com>
2021-12-05 20:14:23 -0500

    try setup from capybara specs

--- spec/features/account_automation_spec.rb
+++ spec/features/account_automation_spec.rb
@@ -1,17 +1,22 @@
 require "spec_helper"
 
 feature "account automation" do
-  scenario "download latest statement" do
+  scenario "view account page, download latest statement" do
     log_in
     screenshot_and_save_page
 
     click_on "Statements"
-    screenshot_and_save_page
 
+    # statement links have target="_blank" attribute
+    window = nil
     within "ul.statement-list-data" do
-      first("a").click
+      window = window_opened_by { first("a").click }
     end
-    screenshot_and_save_page
+
+    within_window window do
+      screenshot_and_save_page
+      puts page.response_headers["Content-Type"]
+    end
 
     log_out
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,26 +4,37 @@ require "webdrivers"
 require "pry"
 require "rspec"
 require_relative "support/authentication_helper"
-# require_relative "support/firefox_driver"
-
-profile = Selenium::WebDriver::Firefox::Profile.new
-profile['browser.download.dir'] = "/home/rd/projects/ally/downloads"
-profile["browser.helperApps.neverAsk.saveToDisk"] = "application/pdf"
-options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
-# driver = Selenium::WebDriver.for(:firefox, options: options)
-Capybara.register_driver(:firefox) do |driver|
-  Capybara::Selenium::Driver.new(driver, browser: :firefox, options: options)
-end
-
-Capybara::Screenshot.register_driver(:firefox) do |driver, path|
-  driver.browser.save_screenshot(path)
-end
 
 Capybara.configure do |config|
   config.run_server = false # capybara standalone mode!
-  config.default_driver = :selenium_headless
+  config.default_driver = :selenium_firefox
   config.app_host = ENV.fetch("WEBSITE_URL")
   config.default_max_wait_time = 10
+  config.save_path = "./tmp/capybara"
+end
+
+# from: https://github.com/teamcapybara/capybara/blob/3.36.0/spec/selenium_spec_firefox.rb
+# doesn't work :(
+browser_options = ::Selenium::WebDriver::Firefox::Options.new
+browser_options.headless! if ENV['HEADLESS']
+
+browser_options.profile = Selenium::WebDriver::Firefox::Profile.new.tap do |profile|
+  profile['browser.download.dir'] = Capybara.save_path
+  profile['browser.download.folderList'] = 2
+  profile['browser.helperApps.neverAsk.saveToDisk'] = 'text/csv,application/pdf,application/octet-stream,text/csv,text/plain'
+  profile['browser.startup.homepage'] = 'about:blank' # workaround bug in Selenium 4 alpha4-7
+  profile['accessibility.tabfocus'] = 7 # make tab move over links too
+end
+
+Capybara.register_driver :selenium_firefox do |app|
+  driver_options = { browser: :firefox, timeout: 31 }.tap do |opts|
+    opts[:capabilities] = browser_options
+  end
+  Capybara::Selenium::Driver.new(app, **driver_options)
+end
+
+Capybara::Screenshot.register_driver(Capybara.default_driver) do |driver, path|
+  driver.browser.save_screenshot(path)
 end
 
 RSpec.configure do |config|
