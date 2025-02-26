ae119a5a26c2bf2c02d1d6b87134e459334447d9
Richard Davis <rdavis.bacs@gmail.com>
2025-02-26 14:56:34 -0500

    Unset user-agent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ Capybara.register_driver :custom_chromium do |app|
   # user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
   # user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:74.0) Gecko/20100101 Firefox/74.0'
   # user_agent = 'Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285'
-  user_agent = 'Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)'
+  # user_agent = 'Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)'
 
   # https://chromium.googlesource.com/chromium/src/+/master/chrome/common/chrome_switches.cc
   [
@@ -33,7 +33,7 @@ Capybara.register_driver :custom_chromium do |app|
     "--disable-dev-shm-usage",
     # "--kiosk",
     # "--window-size=1280,900",
-    "--user-agent=#{user_agent}",
+    # "--user-agent=#{user_agent}",
   ].each { |arg| options.add_argument(arg) }
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
