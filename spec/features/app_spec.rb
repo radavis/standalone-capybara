79482804f1c1875e3b528332bdd5c7d2aac144e9
Richard Davis <rdavis.bacs@gmail.com>
2025-02-26 14:55:03 -0500

    Functional

---
+++ spec/features/app_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+
+feature "sign in, use app" do
+  scenario "user signs in, does a remix" do
+    visit "/"
+
+    at_marketing_page
+    dismiss_cookies_modal
+    sign_in_with_google
+
+    at_root_page
+    click_first_suggestion
+    document_has_rendered
+
+    click_first_remix_suggestion
+    document_has_rendered
+  end
+end
+
+def at_marketing_page
+  expect(page).to have_current_path("/marketing")
+  expect(page).to have_selector("h3", text: /^Create the perfect/i)
+end
+
+def at_root_page
+  expect(page).to have_current_path("/")
+  expect(page).to have_content(/Create Powerful Content/)
+end
+
+def dismiss_cookies_modal
+  within "#cm" do
+    expect(page).to have_content("We use cookies!")
+    click_on "Accept all"
+  end
+end
+
+def click_first_suggestion
+  expect(page).to have_selector(".capitol-suggestions__options")
+
+  within ".capitol-suggestions__options" do
+    find('.capitol-suggestion', match: :first).click
+  end
+
+  within "#remix-sidebar" do
+    expect(page).to have_content("Preparing your document")
+  end
+end
+
+def click_first_remix_suggestion
+  find('#remix-prompt', match: :first).click  # ambiguous match, lol
+
+  within "#prompt-suggestions-container" do
+    find(".capitol-prompt-suggestions__suggestion-item", match: :first).click
+  end
+
+  within "#remix-sidebar" do
+    expect(page).to have_content("Building your document")
+  end
+end
+
+def document_has_rendered
+  within "#remix-sidebar" do
+    expect(page).to have_content("Primary Model", wait: 60)
+  end
+end
+
+def sign_in_with_google
+  click_on "Log in"
+  click_on "Log in with Google"
+
+  find_field(type: 'email').native.send_keys(ENV["WEBSITE_USERNAME"])
+  click_on "Next"
+
+  find_field(type: 'password').native.send_keys(ENV["WEBSITE_PASSWORD"])
+  click_on "Next"
+
+  click_on "Continue"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
-require "capybara/rspec"
-require "selenium-webdriver"
 require "dotenv"
 Dotenv.load
+
+require "capybara/rspec"
+require "selenium-webdriver"
 require "pry"
 require "rspec"
+
+Capybara.run_server = false
 
 Capybara.register_driver :custom_chromium do |app|
   options = Selenium::WebDriver::Chrome::Options.new
@@ -11,9 +14,12 @@ Capybara.register_driver :custom_chromium do |app|
   # user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
   # user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:74.0) Gecko/20100101 Firefox/74.0'
   # user_agent = 'Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285'
+  user_agent = 'Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)'
+
+  # https://chromium.googlesource.com/chromium/src/+/master/chrome/common/chrome_switches.cc
   [
     # "--browser-test",
-    # "--start-maximized",
+    "--start-maximized",
     "--disable-blink-features=AutomationControlled",
     # "--disable-auto-maximize-for-tests",
     # "--disable-background-networking",
@@ -21,23 +27,20 @@ Capybara.register_driver :custom_chromium do |app|
     # "--headless",
     # "--incognito",
     "--no-sandbox",
-    "--disable-infobars",
     # "--disable-web-security",
     # "--allow-running-insecure-content",
     # "--disable-client-side-phishing-detection",
     "--disable-dev-shm-usage",
+    # "--kiosk",
     # "--window-size=1280,900",
-    # "--user-agent=#{user_agent}",
+    "--user-agent=#{user_agent}",
   ].each { |arg| options.add_argument(arg) }
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
-
 Capybara.javascript_driver = :custom_chromium
-Capybara.run_server = false
 
 Capybara.configure do |config|
-  config.default_max_wait_time = 30
   config.default_driver = :custom_chromium # %w[selenium selenium_headless selenium_chrome selenium_headless_chrome]
   config.app_host = ENV["WEBSITE_URL"] || "http://localhost:3000"
 end
